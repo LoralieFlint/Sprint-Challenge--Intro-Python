98c5db5c3f19b66ba3cabbc6069ba15a31140961
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-09-11 18:51:55 -0700

    class inheritance completed

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -50,7 +50,7 @@ print(d)
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{human.name} : {human.age}" for human in humans]
 print(e)
 
 # Write a list comprehension that creates a list of tuples containing name and
@@ -64,7 +64,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [human.name.toupper() for human in humans.name]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
